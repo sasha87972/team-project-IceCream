b64cf478dbc8e7c310f08fcd54f673e1c538e1fd
sasha87972 <82048515+sasha87972@users.noreply.github.com>
2021-07-16 18:45:53 +0000

    Deploying to gh-pages from @ sasha87972/team-project-IceCream@a9345708f79b78fa09240aa630978b918a8bffce 🚀

--- src.7076590f.js
+++ src.7076590f.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-project-IceCream/src.60845a66.js.map
+//# sourceMappingURL=/team-project-IceCream/src.7076590f.js.map
